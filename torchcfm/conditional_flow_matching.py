56281ddb514191096365e289b0152eaf1b13eb42
meet <mparikh2@nd.edu>
2025-10-15 17:53:01 -0400

    add eqm code

--- torchcfm/conditional_flow_matching.py
+++ torchcfm/conditional_flow_matching.py
@@ -979,3 +979,62 @@ class CFM_modified(ConditionalFlowMatcher):
         dsig_t = -self.sigma*torch.exp(-self.scale*t) * (self.scale*(1 - t) + 1)
         dmu_t = x1 - x0
         return (dsig_t/sig_t)[..., None] * (xt - mu_t) + dmu_t
+    
+class EquilibriumMatching :
+    
+    """Equilibrium Matching : https://arxiv.org/pdf/2510.02300
+    """
+    
+    def __init__(self, sched='linear', lamda = 4, **kwargs):
+        
+        self.lamda = lamda
+        if sched =="linear":
+            self.c = lambda t : 1 - t
+        elif sched=="trunc_decay":
+            assert "a" in kwargs.keys(), "missing parameter for truncated decay"
+            self.c = lambda t, a=kwargs["a"] : torch.where(t <= a, 1., (1 - t)/(1 - a)) # 1 if t <= a else (1 - t)/(1 - a) 
+        elif sched=="piecewise":
+            assert "a" in kwargs.keys() and "b" in kwargs.keys(),  "missing parameter(s) for piecewise"
+            self.c = lambda t, a=kwargs["a"], b=kwargs["b"] : b - (b - 1)/a * t if t <= a else (1 - t)/(1 - a)
+        else:
+            AssertionError("the schedule is not implemented")       
+                        
+    def compute_mu_t(self, x0, x1, t):
+        
+        del x0
+        t = pad_t_like_x(t, x1)
+        return t * x1
+
+    def compute_sigma_t(self, t):
+       
+        return  1 - t
+    
+    def sample_noise_like(self, x):
+        return torch.randn_like(x)
+
+    def sample_xt(self, x0, x1, t, epsilon):
+        
+        mu_t = self.compute_mu_t(x0, x1, t)
+        sigma_t = self.compute_sigma_t(t)
+        sigma_t = pad_t_like_x(sigma_t, x1)
+        return mu_t + sigma_t * epsilon
+
+    def compute_conditional_flow(self, eps, x1, t):
+        
+        grad = eps - x1
+        return grad  * pad_t_like_x(self.lamda * self.c(t), x1)
+
+    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False):
+       
+        if t is None:
+            t = torch.rand(x0.shape[0]).type_as(x0)
+        assert len(t) == x0.shape[0], "t has to have batch size dimension"
+
+        eps = self.sample_noise_like(x0)
+        xt = self.sample_xt(x0, x1, t, eps)
+        ut = self.compute_conditional_flow(eps, x1, t)
+        
+        if return_noise:
+            return t, xt, ut, eps
+        else:
+            return t, xt, ut
