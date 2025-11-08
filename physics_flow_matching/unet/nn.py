701246260ede87976c5056a2a2bb19c6d06d6ad5
Mehdi Heydari <mmh287@cornell.edu>
2025-11-07 19:27:54 -0500

    Fix PyTorch gradient checkpointing compatibility
    Use save_for_backward/saved_tensors instead of directly setting
    ctx attributes to fix AttributeError with modern PyTorch versions.

--- physics_flow_matching/unet/nn.py
+++ physics_flow_matching/unet/nn.py
@@ -125,29 +125,32 @@ class CheckpointFunction(th.autograd.Function):
     @staticmethod
     def forward(ctx, run_function, length, *args):
         ctx.run_function = run_function
-        ctx.input_tensors = list(args[:length])
-        ctx.input_params = list(args[length:])
+        ctx.length = length
+        # Save tensors for backward pass
+        ctx.save_for_backward(*args)
         with th.no_grad():
-            output_tensors = ctx.run_function(*ctx.input_tensors)
+            output_tensors = ctx.run_function(*args[:length])
         return output_tensors
 
     @staticmethod
     def backward(ctx, *output_grads):
-        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
+        # Retrieve saved tensors
+        args = ctx.saved_tensors
+        input_tensors = [x.detach().requires_grad_(True) for x in args[:ctx.length]]
+        input_params = list(args[ctx.length:])
+
         with th.enable_grad():
             # Fixes a bug where the first op in run_function modifies the
             # Tensor storage in place, which is not allowed for detach()'d
             # Tensors.
-            shallow_copies = [x.view_as(x) for x in ctx.input_tensors]
+            shallow_copies = [x.view_as(x) for x in input_tensors]
             output_tensors = ctx.run_function(*shallow_copies)
         input_grads = th.autograd.grad(
             output_tensors,
-            ctx.input_tensors + ctx.input_params,
+            input_tensors + input_params,
             output_grads,
             allow_unused=True,
         )
-        del ctx.input_tensors
-        del ctx.input_params
         del output_tensors
         return (None, None) + input_grads
 
